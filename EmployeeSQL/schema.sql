9b706da3c922a4bbe73c1ffb9a802bfa3c098e59
OKnows <114944966+OKnows@users.noreply.github.com>
2023-02-14 20:06:15 -0800

    adding screenshot of erd

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,4 +1,4 @@
---Create titles table
+--Create titles table CORRECT
 CREATE TABLE titles (
  	title_id VARCHAR PRIMARY KEY,
  	title VARCHAR NOT NULL
@@ -6,9 +6,10 @@ CREATE TABLE titles (
 
 --Create employees table
 CREATE TABLE employees (
- 	emp_no INTEGER PRIMARY KEY,
+ 	emp_no INTEGER,
 	emp_title_id VARCHAR,
- 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+	PRIMARY KEY (emp_no, emp_title_id),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	birth_date VARCHAR,
 	first_name VARCHAR,
 	last_name VARCHAR,
@@ -19,7 +20,9 @@ CREATE TABLE employees (
 --Create dept_emp table
 CREATE TABLE dept_emp (
 	emp_no INTEGER,
-	dept_no VARCHAR PRIMARY KEY
+	dept_no VARCHAR,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 --Create departments table
@@ -30,8 +33,9 @@ CREATE TABLE departments (
 
 --Create dept_manager table
 CREATE TABLE dept_manager (
-	dept_no VARCHAR PRIMARY KEY,
-	emp_no INTEGER
+	dept_no VARCHAR,
+	emp_no INTEGER,
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 --Create salaries table
